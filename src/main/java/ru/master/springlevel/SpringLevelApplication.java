af3c002fed22421d6ccafd5bc0cc14a3b56b79ab
igoryasha <stellarnexus@gmail.com>
2021-04-26 22:29:55 +0300

    git init

---
+++ src/main/java/ru/master/springlevel/SpringLevelApplication.java
@@ -0,0 +1,11 @@
+package ru.master.springlevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLevelApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLevelApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/master/springlevel/SpringLevelApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.master.springlevel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLevelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
